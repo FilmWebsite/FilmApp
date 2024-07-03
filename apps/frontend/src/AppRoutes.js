26384a1fa4419e946ff2c2a0bd095adb55cccc91
Doron Reid <doron.r@Dorons-MacBook-Pro.local>
2024-07-03 18:49:35 -0400

    New Updates

--- apps/frontend/src/AppRoutes.js
+++ apps/frontend/src/AppRoutes.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import { Routes, Route, useLocation } from 'react-router-dom';
-import Footer from './comps/Footer';
-import Gallery from './Gallery.tsx';
-import About from './About';
-import RequestPhotos from './RequestPhotos';
-import AlbumEditLogin from './AlbumEditLogin.js';
-import AlbumEdit from './AlbumEdit.js';
-import { Collection } from './Collection.tsx';
+import React from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Footer from "./comps/Footer";
+import Gallery from "./Gallery.tsx";
+import About from "./About";
+import RequestPhotos from "./RequestPhotos";
+import AlbumEditLogin from "./AlbumEditLogin.js";
+import AlbumEdit from "./AlbumEdit.js";
+import { Collection } from "./Collection.tsx";
 import Downloads from "./Downloads.js";
-
 
 function AppRoutes() {
   const location = useLocation();
@@ -16,17 +15,18 @@ function AppRoutes() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Gallery />} />
-        <Route path='/collections/:collection' element={<Collection />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/request-photos' element={<RequestPhotos />} />
-        <Route path='/albums-login' element={<AlbumEditLogin />} />
-        <Route path='/albums-edit' element={<AlbumEdit />} />
+        <Route path="/" element={<Gallery />} />
+        <Route path="/collections/:collection" element={<Collection />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/request-photos" element={<RequestPhotos />} />
+        <Route path="/albums-login" element={<AlbumEditLogin />} />
+        <Route path="/albums-edit" element={<AlbumEdit />} />
+        <Route path="/downloads" element={<Downloads />} />
       </Routes>
-      {location.pathname !== '/about' &&
-        location.pathname !== '/albums-login' &&
-        location.pathname !== '/albums-edit' &&
-        location.pathname !== '/page' && <Footer />}
+      {location.pathname !== "/about" &&
+        location.pathname !== "/albums-login" &&
+        location.pathname !== "/albums-edit" &&
+        location.pathname !== "/page" && <Footer />}
     </>
   );
 }
