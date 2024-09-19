d1f44927ce946a7ef0c8bbe81f8c57a7ab44e5f2
Doron Reid <doron.r@dorons-mbp.mynetworksettings.com>
2024-09-19 13:29:17 -0400

    Scroll change

--- apps/frontend/src/Gallery.tsx
+++ apps/frontend/src/Gallery.tsx
@@ -72,20 +72,55 @@ type ScrollProps = {
   collections: any[];
 };
 
+
 const HorizontalScrollCarousel = (props: ScrollProps) => {
   const targetRef = useRef(null);
+  const scrollContainerRef = useRef<HTMLElement>(null);
   const { scrollYProgress } = useScroll({
     target: targetRef,
   });
 
-  const x = useTransform(scrollYProgress, [0, 1], ['1%', '-40%']);
+  const [dynamicOffset, setDynamicOffset] = useState('-2%');
+  const [scrollLimit, setScrollLimit] = useState('-2%');
+
+  useEffect(() => {
+    const handleResize = () => {
+      const screenWidth = window.innerWidth;
+      const containerWidth = scrollContainerRef.current
+        ? scrollContainerRef.current.scrollWidth
+        : 0;
+      const calculatedOffset = screenWidth > 1200 ? '-100%' : '-80%';
+      setDynamicOffset(calculatedOffset);
+
+      // Calculate how much to scroll based on container width and screen width
+      const limit = screenWidth - containerWidth;
+      setScrollLimit(`${limit}px`);
+    };
+
+    // Initial calculation
+    handleResize();
+
+    // Add event listener for window resize
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup event listener on component unmount
+    return () => window.removeEventListener('resize', handleResize);
+  }, [props.collections]);
+
+  // Adjust scroll end point using scrollLimit
+  const x = useTransform(scrollYProgress, [0, 1], ['1%', scrollLimit]);
+
   return (
     <div>
-      <section ref={targetRef} className='relative h-[250vh] '>
+      <section ref={targetRef} className='relative h-[250vh]'>
         <div className='sticky top-0 flex h-screen items-center overflow-hidden'>
-          <motion.div style={{ x }} className='flex gap-4 mt-[-80px]'>
+          <motion.div
+            ref={scrollContainerRef}
+            style={{ x }}
+            className='flex gap-4 mt-[-80px]'
+          >
             {props.collections.map((card) => {
-              return <CollectionCard card={card} />;
+              return <CollectionCard key={card.id} card={card} />;
             })}
           </motion.div>
         </div>
@@ -93,6 +128,7 @@ const HorizontalScrollCarousel = (props: ScrollProps) => {
     </div>
   );
 };
+
 
 const CollectionCard = ({ card }: { card: Collection }) => {
   return (
