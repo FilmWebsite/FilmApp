178c0c8934814bd5354080a5c3202b35851fe2ee
Doron Reid <doron.r@Dorons-MBP.attlocal.net>
2025-01-09 13:50:22 -0600

    Admin Updates

--- apps/frontend/src/components/EditCollection.tsx
+++ apps/frontend/src/components/EditCollection.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Collection } from '@film/photos-iso';
-import { EditImageRow } from './EditImageRow';
 import { usePhotos } from '@film/photos-web';
 import '../styles/Admin.scss';
+import { LuSwitchCamera } from 'react-icons/lu';
+import { MdDelete } from "react-icons/md";
+import { FaPhotoFilm } from "react-icons/fa6";
+import { IoMdAdd } from 'react-icons/io';
 // import { IoChevronBackOutline } from 'react-icons/io5';
 
 type EditCollectionProps = {
@@ -23,65 +26,109 @@ const EditCollection = ({
       <a onClick={() => setSelectedCard(null)}>Back</a>
 
       <div className='selectedTop'>
-        <div className='relative h-[350px] w-full max-w-[350px] overflow-hidden cursor-pointer'>
-          <img
-            src={selectedCard.cover_image}
-            alt={selectedCard.card_name}
-            className='absolute inset-0 z-0 object-cover w-full h-full transition-transform duration-300 group-hover:scale-110'
-            onError={(e) => {
-              e.currentTarget.src = '/path/to/fallback-image.jpg';
-            }}
-          />
+
+        <div className="selectedAlbumImg">
+            <div className="relative h-[350px] w-full max-w-[350px] overflow-hidden cursor-pointer">
+                <img
+                src={selectedCard.cover_image}
+                alt={selectedCard.card_name}
+                className="photoImage"
+                onError={(e) => {
+                    e.currentTarget.src = '/path/to/fallback-image.jpg';
+                }}
+                />
+            </div>
+            <LuSwitchCamera className="selectedAlbumIcon" />
         </div>
 
         <div className='selectedTopInfo'>
-          <h1 className='selectedTitle'>{selectedCard.card_name}</h1>
+            <h1 className='selectedTitle'>
+            <input
+                type='text'
+                className='editBox'
+                defaultValue={selectedCard.card_name}
+                placeholder='Enter card name'
+            />
+            </h1>
 
-          <div className='selectedHeaders'>
-            <div className='selectedColorsInput'>
-              <p className='inputHeader'>Text Color</p>
-              <p>{selectedCard.colors.textColor}</p>
+            <div className='selectedHeaders'>
+                <div className='selectedColorsInput'>
+                    <p className='inputHeader'>Text Color</p>
+                    <input
+                    type='text'
+                    className='editBox'
+                    defaultValue={selectedCard.colors.textColor}
+                    placeholder='Enter text color'
+                    />
+                </div>
+
+                <div className='selectedColorsInput'>
+                    <p className='inputHeader'>Shadow Color</p>
+                    <input
+                    type='text'
+                    className='editBox'
+                    defaultValue={selectedCard.colors.shadowColor}
+                    placeholder='Enter shadow color'
+                    />
+                </div>
             </div>
 
-            <div className='selectedColorsInput'>
-              <p className='inputHeader'>Shadow Color</p>
-              <p>{selectedCard.colors.shadowColor}</p>
-            </div>
-          </div>
+            <div className='selectedHeaders'>
+                <div className='selectedDisplayName'>
+                    <p className='inputHeader'>Display Name</p>
+                    <input
+                    type='text'
+                    className='editBox'
+                    defaultValue={selectedCard.display_name || 'no display name'}
+                    placeholder='Enter display name'
+                    />
+                </div>
 
-          <div className='selectedHeaders'>
-            <div className='selectedDisplayName'>
-              <p className='inputHeader'>Display Name</p>
-              <p>{selectedCard.display_name || 'no display name'}</p>
+                <div className='selectedDisplayName'>
+                    <p className='inputHeader'>Ref</p>
+                    <input
+                    type='text'
+                    className='editBox'
+                    defaultValue={selectedCard.ref || 'no ref name'}
+                    placeholder='Enter reference'
+                    />
+                </div>
             </div>
 
-            <div className='selectedDisplayName'>
-              <p className='inputHeader'>Ref</p>
-              <p>{selectedCard.ref || 'no ref name'}</p>
-            </div>
-          </div>
+            <p className='inputHeader'>Description</p>
+            <textarea
+            className='editDesc'
+            defaultValue={selectedCard.desc || 'coming soon...'}
+            placeholder='Enter description'
+            />
 
-          <p className='inputHeader'>Description</p>
-          <p className='selectedDesc'>
-            {selectedCard.desc || 'coming soon...'}
-          </p>
+            </div>
         </div>
-      </div>
 
-      <div className='grid grid-cols-4 gap-8 justify-center'>
-        <div>{selectedCard.card_name}</div>
-      </div>
+        <div className="photoGrid">
+            {photos.map((pics, index) => (
+                <div key={index} className="photoSection">
+                    <div key={index} className="relative h-[300px] w-full max-w-[300px] overflow-hidden">
+                        <img
+                            src={pics.url}
+                            alt="grid item"
+                            className="collectionPhotos"
+                        />
+                    </div>
+                    <div className='photoOptions'>
+                        <FaPhotoFilm className='photoSwitch' />
+                        <MdDelete className='photoDelete' />
+                    </div>
+                    
+                </div>
+                
+            ))}
+            <div className="addPhoto">
+                <IoMdAdd className='add' />
+            </div>
+        </div>
 
-      <div className='grid grid-cols-4 gap-8 justify-center'>
-        {photos.map((p, index) => (
-          <img
-            key={index} // Provide a unique key for each item
-            src={p.url}
-            alt='grid item'
-            // style={{ visibility: 'hidden', position: 'absolute' }} // Hide img tag
-          />
-        ))}
-      </div>
+
     </div>
   );
 };
